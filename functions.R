63268bebcb82c49388ec0283ed6eccc8afcd0ea4
James Watson <jwatowatson@gmail.com>
2024-04-16 09:12:56 +0700

    minor updates

--- functions.R
+++ functions.R
@@ -15,28 +15,26 @@ make_matrix_pcr = function(pcr_dat, N_max_PCR=NA){
     ind = which(pcr_dat$ID==id)
     pcr_mat[id, pcr_dat$PCR_name[ind]] = pcr_dat$CT[ind]
   }
+  return(pcr_mat)
   
+}
+plot_pcr_matrix = function(pcr_mat, my_breaks, my_break_legend, my_cols, 
+                           visit_labels, arm_labels,
+                           ind_y){  
   image(t(pcr_mat), x=1:ncol(pcr_mat), y = 1:nrow(pcr_mat),
-        breaks = c(10, 34, 36, 38, 39.99999,40),
-        col = (c(brewer.pal(4, 'RdYlBu'),brewer.pal(9, 'Pastel1')[9])),
+        breaks = my_breaks,
+        col = my_cols,
         xlab='', xaxt = 'n', yaxt='n', ylab='')
-  legend('topright', inset= c(-0.2,0), title = 'Mean CT',
-         fill= c(brewer.pal(4, 'RdYlBu'),'lightgrey'), xpd=T,
-         legend = c('<34','34-36','36-38','38-40','>=40'))
+  legend('topright', inset= c(-0.1,0), title = 'CT',
+         fill= my_cols, xpd=T,
+         legend = my_break_legend)
   axis(1, at = (1:(ncol(pcr_mat)/9))*9 - 4.5, 
-       labels = gsub(x = unique(pcr_dat$VISIT),
-                     pattern = '(',replacement = '\n(',fixed = T), las=2)
-  xx2 = pcr_dat %>% distinct(ID,.keep_all = T)
-  ind_y = which(!duplicated(xx2$ARM))-0.5
+       labels = visit_labels, las=2)
+  
   abline(h=ind_y[-1], lty=2)
   
   abline(v=(1:(ncol(pcr_mat)/9))*9 + 0.5, lty=2)
-  
-  my_labs=unique(pcr_dat$ARM)
-  my_labs=gsub(pattern = 'BENZNIDAZOLE',replacement = 'BNZ',x = my_labs)
-  my_labs=gsub(pattern = 'FOSRAVUCONAZOLE',replacement = '\nE1224',x = my_labs)
-  my_labs=gsub(pattern = ' WEEKLY',replacement = '/WK',x = my_labs)
-  
-  axis(2, at = ind_y+15, labels = my_labs,las=1,tick = F)
+
+  axis(2, at = ind_y+15, labels = arm_labels,las=1,tick = F)
   
 }
